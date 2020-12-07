ae8c6ad6aa6cee7f60a21dbd2e2ac3ac05f6af34
Vikas7kr <rogetsgeneral7@gmail.com>
2020-12-07 12:36:21 +0530

    react-todo

---
+++ build/precache-manifest.d890197441d81684eab11e461eb328b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cc1a81f2f0e0641dbb0ec58ad50f29",
+    "url": "/daily/index.html"
+  },
+  {
+    "revision": "cec1aa8830d6f89b293a",
+    "url": "/daily/static/css/main.21df692d.chunk.css"
+  },
+  {
+    "revision": "ad33dd02a7d804c73be2",
+    "url": "/daily/static/js/2.4d7e6d97.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/daily/static/js/2.4d7e6d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec1aa8830d6f89b293a",
+    "url": "/daily/static/js/main.ad5018f7.chunk.js"
+  },
+  {
+    "revision": "90932acc1fa88521c4e3",
+    "url": "/daily/static/js/runtime-main.44f87bbf.js"
+  }
+]);
